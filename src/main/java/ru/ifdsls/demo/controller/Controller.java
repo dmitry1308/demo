17eb18c4a963f98bb5b93ee17170d9c33c77b15d
Schepin Dmitriy <bright1308@gmail.com>
2020-10-27 18:51:36 +0300

    Initial commit

---
+++ src/main/java/ru/ifdsls/demo/controller/Controller.java
@@ -0,0 +1,14 @@
+package ru.ifdsls.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/validate/prime-number")
+    public String isNumberPrime(@RequestParam("number") Integer number) {
+        return Integer.parseInt(String.valueOf(number)) % 2 == 0 ? "Even" : "Odd";
+    }
+}
